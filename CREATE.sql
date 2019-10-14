0f04e9e67bb151093f63cfd21a58d59ea1c6cc3d
Diego J. Moguer Castillo <diego_dj_m@hotmail.com>
2019-10-14 09:59:19 +0200

    Eliminación error

--- CREATE.sql
+++
@@ -1,8 +0,0 @@
-create table AlumnosDiego
-(
-    codigo_alu number(4) primary key,
-    nombre varchar2(50) not null,
-    apellidos varchar2(50),
-    edad number(2),
-    direccion varchar2(100)
-);
